2a04d2be73c1976b24b60c5724d0fe40c6e3989f
Casino <vipnormalhy@outlook.com>
2018-10-10 13:02:16 +0800

    initial commit

---
+++ Source/Learn2D/Learn2D.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "Learn2D.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Learn2D, "Learn2D" );
