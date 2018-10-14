5a46b1147f03e36f73df978404f8298fa81c26b9
Casino <vipnormalhy@gmail.com>
2018-10-14 16:31:37 +0800

    fix include file order

--- Source/ForGina/Charactor/SpriteCharactor.cpp
+++ Source/ForGina/Charactor/SpriteCharactor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/SceneComponent.h"
 #include "SpriteCharactor.h"
+#include "Components/SceneComponent.h"
 
 
 // Sets default values
